e60f8ff6ed54989fadf45394640694217882ab7b
tom-newbold <thomas_newbold@outlook.com>
2022-10-27 14:47:19 +0100

    _

--- CardGame.java
+++ CardGame.java
@@ -1,9 +1,15 @@
 public class CardGame {
     /**
      * Takes input, checks validity
+     * Creates n Player threads
      * Distributes cards, inital win condition check, distribute decks
      */
     public static void main(String[] args) {
-        // main loop
+        // placeholder n
+        int n = 4;
+        Thread[] threads = new Thread[4];
+        for(int i=0;i<n;i++) {
+            threads[i] = new Thread(); // anon class in here?
+        }
     }
 }
--- Player.java
+++ Player.java
@@ -2,11 +2,13 @@ import java.util.LinkedList;
 
 interface PlayerInterface {
     public void drawCard();
-    public void discardCard();
+    public Card discardCard();
     public Boolean winCondition();
 }
 
 public class Player {
     private LinkedList<Card> cards;
     private Integer preferedDenom;
+    // also pass in decks
+    public Player(Card[] hand) {}
 }
