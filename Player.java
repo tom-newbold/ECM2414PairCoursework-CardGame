e6b7857ac5439fe10e8e6b3dfef2c42161a1d650
tom-newbold <thomas_newbold@outlook.com>
2022-11-08 13:25:15 +0000

    mostly implemented player class

--- Player.java
+++ Player.java
@@ -2,25 +2,63 @@ import java.util.LinkedList;
 
 public class Player {
     private Integer playerID;
-    private LinkedList<Card> cards;
-    private Integer preferedDenom;
-    public Player(Integer pID, Card[] hand) {}
+    private LinkedList<Card> hand;
+    private Integer preferredDenom;
+    public Player(Integer pID, Card[] cards) {
+        this.playerID = pID;
+        this.preferredDenom = pID;
+        for (Card c : cards) {
+            this.hand.add(c);
+        }
+    }
 
     /**
      * Draws card from the referenced deck and adds to Players hand
      * @param d The deck to draw from
      */
-    public void drawCard(Deck d) {}
+    private void drawCard(Deck d) throws InterruptedException {
+        this.hand.add(d.drawTopCard());
+    }
 
     /**
      * Chooses and discards a card from the Player's hand (not of prefered denomination)
      * @returns The discarded card
      */
-    public Card discardCard() { return new Card(0); }
+    private Card discardCard() {
+        Card toDiscard;
+        int maxAge = -1;
+        for (Card c : this.hand) {
+            if(c.getValue() != this.preferredDenom && c.getAge()>maxAge) {
+                toDiscard = c;
+                maxAge = c.getAge();
+            }
+        }
+        // toDiscard should always be defined ????????
+        return this.hand.pop(); // TODO: return c
+    }
 
     /**
-     * Checks the players cards to see if they have a winning hand
+     * Performs a players full turn atomically (before checking win condition);
+     * Player's hand will only ever contain 4 cards outside of this function
+     * @param d1 The deck to draw from
+     * @param d2 The deck to discard to
+     */
+    public void atomicTurn(Deck d1, Deck d2) throws InterruptedException {
+        this.drawCard(d1);
+        d2.addCard(this.discardCard());
+    }
+
+    /**
+     * Checks the players cards to see if
+     * Does not have to be preferred they have a winning hand
      * @return True if the player has a winning hand
      */
-    public Boolean winCondition() { return false; }
+    public Boolean winCondition() {
+        /*
+        if (hand.contains(preferedDenom)) {
+            
+        }
+        */
+        return false; 
+    } // TODO
 }
