4f2c6bbe6b4592ec28e606f3c054b6d09debd6af
tom-newbold <thomas_newbold@outlook.com>
2022-11-08 12:51:21 +0000

    implemented card class

--- Card.java
+++ Card.java
@@ -1,25 +1,27 @@
 public class Card {
     private Integer value;
-    private Integer age = 0;
-    public Card(int value) {}
+    private Integer age;
+    public Card(Integer v) {
+        this.value = v;
+        resetAge();
+    }
 
     /**
      * Only getter method; value is read-only so no syncronisation required
      * @return The value of the card
      */
-
-    public Integer getValue() { return 0; }
+    public Integer getValue() { return this.value; }
 
     /**
      * @return The "age" of the card (number of turns it has been in the players hand)
      */
-    public Integer getAge() { return 0; }
+    public Integer getAge() { return this.age; }
     /**
      * "Ages" the card; used to prevent stale hands
      */
-    public void age() {} // age++;
+    public void age() { this.age++; }
     /**
      * Sets the age to zero (when a card is picked up from the deck)
      */
-    public void resetAge() {}
+    public void resetAge() { this.age = 0; }
 }
