4a7fac53dbb5bc58ae1195477de9b859a8d4bd64
tom-newbold <thomas_newbold@outlook.com>
2022-11-08 13:03:09 +0000

    implemented deck class

--- Deck.java
+++ Deck.java
@@ -1,18 +1,30 @@
 import java.util.LinkedList;
 
 public class Deck {
-    private LinkedList<Card> deck;
-    public Deck(Card[] cards) {}
+    private LinkedList<Card> deck = new LinkedList<Card>();
+    public Deck(Card[] cards) {
+        for (Card c : cards) {
+            this.deck.add(c);
+        }
+    }
 
     /**
      * Draws the top card from the deck
      * @return The drawn card
      */
-    public synchronized Card drawTopCard() {return new Card(0); };
+    public synchronized Card drawTopCard() throws InterruptedException {
+        while(this.deck.size()<=0) {
+            wait();
+        }
+        return this.deck.pop();
+    };
 
     /**
      * Adds a card to the (bottom of?) the deck
      * @param c The card to be added to the deck
      */
-    public synchronized void addCard(Card c) {};
+    public synchronized void addCard(Card c) {
+        this.deck.add(c);
+        notify();
+    };
 }
