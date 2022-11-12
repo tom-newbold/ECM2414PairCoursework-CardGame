9caaf6567062ae8ce982390c566019ec0cb117b6
StevenXD777 <stevenjangcan2003@gmail.com>
2022-11-12 01:25:22 +0000

    playerinput, fileinput, thread and pack creation

--- CardGame.java
+++ CardGame.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 
 public class CardGame {
     /**
@@ -7,35 +11,75 @@ public class CardGame {
      * Distributes cards, inital win condition check, distribute decks
      * Start threads
      */
+
+    /* TASKS TO DO:
+     * *DONE* player input and test validity 
+     * *DONE* pack file input and validity
+     * player threads created
+     * new card object created and written to pack file
+     * cards are distributed to make list of hands and decks for each player
+     * output file is made for players and the decks EACH
+     * wincondition checked upon each player, if no win is made
+     * atomic action of drawing, checking win condition and discarding for player threads
+     * atomic action of drawing from top of deck and add from bottom of deck
+     * wincondition is met and player that has one notifies other players so game is stopped
+     */
+
+    public static Integer players;
+    public static String packFile;
+
     public static void main(String[] args) {
-        Integer players;
 
-        System.out.println("Enter the number of players:");
-        Boolean validity = false;
-
-        while(validity == false) {
-            Scanner sc = new Scanner(System.in);
-            if(sc.hasNextInt()) {
-                players = sc.nextInt();
-                if(players > 0) {
-                    validity = true;
-                    sc.close();
-                } else {
-                    System.out.println("Please enter a positive number of players:");
-                }
-            } else {
+        Scanner playerInput = new Scanner(System.in);
+        // Will loop until input is an integer and is postive
+        do {
+            System.out.println("Please enter a positive number of players:");
+            while (!playerInput.hasNextInt()) {
                 System.out.println("Enter a valid number of players:");
+                playerInput.next(); // Allows another input and is checked against conditions
             }
-        }
+            players = playerInput.nextInt();
+        } while (players < 0);
+        //playerinput.close();
         
-        /*
+        Scanner fileInput = new Scanner(System.in); // Second scanner object needed for file input
+        Boolean fileExists = false;
+        // input is used for file object creation to check if file exists, if not it will loop
+        do {
+            System.out.println("Enter a valid location of pack to load:");
+            packFile = fileInput.nextLine();
+            File f = new File(packFile);
+            if (f.exists()) {
+                fileExists = true;
+            }
+        } while (fileExists == false);
+        fileInput.close();
+        
+        // main loop for creating threads, make into List of threads?
+        Thread[] threads = new Thread[players];
         for (int i = 0; i < players; i++) {
-            new Thread(Integer.toString(i)) {
-                public void run() {
-                    System.out.println("Player " + this.getName());
-                }
-            }.start();
-        } */
+            threads[i] = new Thread();
+            threads[i].start();
+            System.out.println("Player: " + threads[i].getName());
+        }
+
+        Integer numberOfCards = 8 * players;
+        Random rand = new Random();
+        try {
+            Integer valueRange = 15; // Card Value range from 1 to 15
+            FileWriter writeFile = new FileWriter(packFile);
+            for (Integer i = 0; i < numberOfCards; i++) {
+                Card newCard = new Card(rand.nextInt(valueRange) + 1);
+                writeFile.write(newCard.getValue()); // text file has red blocks???
+                System.out.println(newCard.getValue());
+            }
+            writeFile.close();
+            System.out.println("Pack is set.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
 }
 
@@ -47,6 +91,13 @@ Thread[] threads = new Thread[4];
 for(int i=0;i<n;i++) {
     threads[i] = new Thread(); // anon class in here?
 }
-
  */
 
+/*
+        for (int i = 0; i < players; i++) {
+            new Thread(Integer.toString(i)) {
+                public void run() {
+                    System.out.println("Player " + this.getName());
+                }
+            }.start();
+        } */
