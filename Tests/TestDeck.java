86152d3717eb70cd6ec6b43064191597b41e512b
tom-newbold <thomas_newbold@outlook.com>
2022-11-18 14:59:49 +0000

    added some test classes

--- Tests/TestDeck.java
+++ Tests/TestDeck.java
@@ -1,5 +1,33 @@
 package Tests;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import CardGame.Deck;
+import CardGame.Card;
 
 public class TestDeck {
-    
+    @Test
+    public void testDrawTopCard() throws InterruptedException {
+        Card[] cards = new Card[4];
+        for(Integer i=0;i<4;i++) { cards[i] = new Card(i+1); }
+        Deck deck = new Deck(cards);
+        assertEquals("drawTopCard() failed", cards[0], deck.drawTopCard());
+    }
+
+    @Test
+    public void testAddCard() {
+        Card[] cards = new Card[4];
+        for(Integer i=0;i<4;i++) { cards[i] = new Card(i+1); }
+        Deck deck = new Deck(cards);
+        deck.addCard(new Card(5));
+        assertEquals("addCard() failed", 5, deck.getDeck().length);
+    }
+
+    @Test
+    public void testGetDeck() {
+        Card[] cards = new Card[4];
+        for(Integer i=0;i<4;i++) { cards[i] = new Card(i+1); }
+        Deck deck = new Deck(cards);
+        assertNotEquals("getDeck() failed", cards, deck.getDeck());
+    }
 }
--- Tests/TestPlayer.java
+++ Tests/TestPlayer.java
@@ -1,5 +1,48 @@
 package Tests;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.channels.InterruptedByTimeoutException;
+
+import CardGame.Card;
+import CardGame.Deck;
+import CardGame.Player;
 
 public class TestPlayer {
-    
+    @Test
+    public void testDrawCard() throws InterruptedException {
+        Card[] cards = new Card[4];
+        for(Integer i=0;i<4;i++) { cards[i] = new Card(i+1); }
+        Card[] hand = new Card[4];
+        for(Integer i=0;i<4;i++) { hand[i] = new Card(i+1); }
+        Deck deck = new Deck(cards);
+
+    }
+
+    @Test
+    public void testDiscardCard() {}
+
+    @Test
+    public void testAtomicTurn() {}
+
+    @Test
+    public void testWinCondition() throws IOException {
+        Card[] losingCards = new Card[4];
+        for(Integer i=0;i<4;i++) { losingCards[i] = new Card(i+1); }
+        FileWriter f = new FileWriter("test_out.txt",true);
+        Player p = new Player(f, f, 0, losingCards);
+        assertFalse("winCondition() failed", p.winCondition());
+        
+        Card[] winningCards = new Card[4];
+        for(Integer i=0;i<4;i++) { winningCards[i] = new Card(1); }
+        Player q = new Player(f, f, 1, winningCards);
+        assertTrue("winCondition() failed", q.winCondition());
+        
+        
+    }
+
+    @Test
+    public void testgetHand() {}
 }
