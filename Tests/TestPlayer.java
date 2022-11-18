888db3b98cb94afef822c483b97e029269f7ca18
tom-newbold <thomas_newbold@outlook.com>
2022-11-18 15:51:46 +0000

    more tests Player/PlayerThread (some need fixing)

--- Tests/TestPlayer.java
+++ Tests/TestPlayer.java
@@ -10,35 +10,23 @@ import CardGame.Deck;
 import CardGame.Player;
 
 public class TestPlayer {
-    /*
-    @Test
-    public void testDrawCard() throws InterruptedException, IOException {
-        Card[] cards = new Card[4];
-        for(Integer i=0;i<4;i++) { cards[i] = new Card(i+1); }
-        Card[] hand = new Card[4];
-        for(Integer i=0;i<4;i++) { hand[i] = new Card(i+1); }
-        FileWriter f = new FileWriter("test_out.txt",true);
-        Player p = new Player(f, f, 0, hand);
-
-    }
-
-    @Test
-    public void testDiscardCard() {}
+    /* Cannot test drawCard() or discardCard() methods 
+     * as they private so tests done in atomicTurn() method 
      */
 
     @Test
     public void testAtomicTurn() throws IOException, InterruptedException {
-        Card[] cards = new Card[4];
-        for(Integer i=0;i<4;i++) { cards[i] = new Card(i+1); }
+        Card[] cards = new Card[]{new Card(1), new Card(2)};
         Deck d1 = new Deck(cards);
-        Deck d2 = new Deck(new Card[0]);
+        Deck d2 = new Deck(cards);
         Card[] hand = new Card[4];
         for(Integer i=0;i<4;i++) { hand[i] = new Card(i+1); }
-        FileWriter f = new FileWriter("test_out.txt",true);
-        Player p = new Player(f, f, 2, hand);
+        FileWriter f = new FileWriter("test_out.txt");
+        Player p = new Player(f, f, 1, hand);
         p.atomicTurn(d1, d2);
-        assertEquals("Draw unsuccessful", d1.getDeck(), 3);
-        assertEquals("Discard unsuccessful", d2.getDeck().length, 1); 
+        assertEquals("Draw unsuccessful", d1.getDeck().length, 1);
+        assertEquals("Discard unsuccessful", d2.getDeck().length, 3); 
+        assertNotEquals(1,(int)d2.getDeck()[d2.getDeck().length-1].getValue());
         //assertEquals(p, p);    
     }
 
