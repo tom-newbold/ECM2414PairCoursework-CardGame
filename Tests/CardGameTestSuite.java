38d32fbd2dea141bcf20b20479ad09981a9b6be0
tom-newbold <thomas_newbold@outlook.com>
2022-11-22 17:39:47 +0000

    fixing cardgame test duplicate

--- Tests/CardGameTestSuite.java
+++ Tests/CardGameTestSuite.java
@@ -1,6 +1,6 @@
 package Tests;
 
-import org.junit.BeforeClass;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
--- Tests/TestCardGame.java
+++ Tests/TestCardGame.java
@@ -1,11 +1,11 @@
 package Tests;
 
-import org.junit.AfterClass;
-import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
@@ -13,7 +13,7 @@ import CardGame.CardGame;
 
 public class TestCardGame {
     @Test
-    public void testCardGameDuplicate() {
+    public void testCardGameDuplicate() throws IOException {
         String textInputs = "8\npack.txt\nbin/pack_8player.txt";
         InputStream testInput = new ByteArrayInputStream(textInputs.getBytes(StandardCharsets.UTF_8));
         System.setIn(testInput);
@@ -21,11 +21,12 @@ public class TestCardGame {
         assertNotEquals(0, (int)CardGame.winPlayer);
         assertEquals(8, (int)CardGame.players);
         assertEquals("bin/pack_8player.txt", CardGame.packFile);
-        //close scanner?
+        testInput.close();
+        CardGame.winPlayer = 0;
     }
 
     @Test
-    public void testCardGame() {
+    public void testCardGame() throws IOException {
         String textInputs = "4\nnotapack.txt\nbin/pack.txt";
         InputStream testInput = new ByteArrayInputStream(textInputs.getBytes(StandardCharsets.UTF_8));
         System.setIn(testInput);
@@ -33,5 +34,12 @@ public class TestCardGame {
         assertNotEquals(0, (int)CardGame.winPlayer);
         assertEquals(4, (int)CardGame.players);
         assertEquals("bin/pack.txt", CardGame.packFile);
+        testInput.close();
+        CardGame.winPlayer = 0;
+    }
+
+    @After
+    public void restore() {
+        System.setIn(System.in);
     }
 }
