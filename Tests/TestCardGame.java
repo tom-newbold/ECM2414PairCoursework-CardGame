e0035217bcee0ae5f68538c82f9b89b50ff8c11f
tom-newbold <thomas_newbold@outlook.com>
2022-11-22 11:58:43 +0000

    test suite

--- Tests/TestCardGame.java
+++ Tests/TestCardGame.java
@@ -1,5 +1,10 @@
 package Tests;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestCard.class, TestDeck.class, TestPlayer.class, TestPlayerThread.class})
 public class TestCardGame {
-    
+    // Simply a container for other test classes    
 }
