13a99b19058068c4f1d88f9c584527946efe83ea
tom-newbold <thomas_newbold@outlook.com>
2022-11-22 13:23:21 +0000

    ?

--- Tests/TestCardGame.java
+++ Tests/TestCardGame.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
 import java.io.StringReader;
 
 import CardGame.CardGame;
@@ -13,11 +14,12 @@ import CardGame.CardGame;
 @Suite.SuiteClasses({TestCard.class, TestDeck.class, TestPlayer.class, TestPlayerThread.class})
 public class TestCardGame {
     // Simply a container for other test classes
+    
     @Test
     public void testCardGame() {
-        CardGame cg = new CardGame();
-        System.setIn(new StringReader("4\npack.txt"));
-        cg.main();
+        StringReader testInput = new InputStream(new StringReader("4\npack.txt"));
+        System.setIn(testInput);
+        CardGame.main(null);
         assertNotEquals(0, (int)CardGame.winPlayer);
         assertEquals(4, (int)CardGame.players);
         assertEquals("pack.txt", CardGame.packFile);
